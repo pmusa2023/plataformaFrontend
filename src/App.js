194bb1860901427dca9b741b677a2a34a6c8ee37
pmusa2023 <projectmusa2023@gmail.com>
2023-09-16 21:43:05 +0100

    add: router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Root from './Pages/Root';
+import Home from './Pages/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+
+    children: [
+      {
+        index: true,
+        element: <Home />,
+        id: 'home',
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <RouterProvider router={router} />
+    </React.Fragment>
   );
 }
 
---
+++ src/Pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <React.Fragment>
+      <p>Hello world</p>
+    </React.Fragment>
+  );
+}
+export default Home;
